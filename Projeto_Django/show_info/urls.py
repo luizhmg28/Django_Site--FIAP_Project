0e4e1cd2eae8280a3b0f26e80b8e95679fab7dae
Luiz Henrique Macedo Graça <luizhmg28@gmail.com>
2025-09-14 13:49:14 -0300

    alexa path

--- Projeto_Django/show_info/urls.py
+++ Projeto_Django/show_info/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('alexa/', views.alexa_data, name='alexa_data'),
 ]
--- Projeto_Django/show_info/views.py
+++ Projeto_Django/show_info/views.py
@@ -1,6 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from datetime import date, timedelta
 from .sems_api import crosslogin, get_inverter_data
+
 
 def index(request):
     inverter_sn = "5010KETU229W6177"
@@ -34,3 +36,36 @@ def index(request):
         "data": ontem.strftime("%d/%m/%Y")
     }
     return render(request, "show_info/index.html", contexto)
+
+def alexa_data(request):
+    inverter_sn = "5010KETU229W6177"
+    account = "demo@goodwe.com"
+    password = "GoodweSems123!@#"
+    
+    # Data de ontem
+    ontem = date.today() - timedelta(days=1)
+    dt_str = ontem.strftime("%Y-%m-%d 00:00:00")
+    
+    dados = {}
+    try:
+        token = crosslogin(account, password, "us")
+        for col in ["Pac", "Eday", "Cbattery1"]:
+            resp = get_inverter_data(token, inverter_sn, col, dt_str, "eu")
+            lista = resp.get("data", {}).get("column1", [])
+            if lista:
+                dados[col] = lista[-1].get("column")
+            else:
+                dados[col] = "N/A"
+    except Exception as e:
+        dados = {"Pac": "Erro", "Eday": str(e), "Cbattery1": "Erro"}
+
+    # Monta o JSON que a Alexa vai ler
+    alexa_json = {
+        "inverter_sn": inverter_sn,
+        "data": ontem.strftime("%d/%m/%Y"),
+        "Pac": dados.get("Pac"),
+        "Eday": dados.get("Eday"),
+        "Cbattery1": dados.get("Cbattery1")
+    }
+    
+    return JsonResponse(alexa_json)
